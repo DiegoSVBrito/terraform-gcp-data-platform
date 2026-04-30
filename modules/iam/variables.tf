e00a5e3e030fc342be75523dbab976017645b871
Diego Santiago Vieira de Brito <126785055+DiegoSVBrito@users.noreply.github.com>
2026-04-30 09:39:48 -0400

    feat(iam): variaveis do modulo

---
+++ modules/iam/variables.tf
@@ -0,0 +1,5 @@
+variable "project_id"  { type = string }
+variable "env"         { type = string }
+variable "dataset_id"  { type = string }
+variable "bucket_name" { type = string }
+variable "topic_name"  { type = string }
