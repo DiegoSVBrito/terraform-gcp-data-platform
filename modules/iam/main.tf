e414547ce5500a3a0d325cc2c1673810ed4e120c
Diego Santiago Vieira de Brito <126785055+DiegoSVBrito@users.noreply.github.com>
2026-04-30 09:39:47 -0400

    feat(iam): service account com least-privilege

---
+++ modules/iam/main.tf
@@ -0,0 +1,29 @@
+resource "google_service_account" "data_platform" {
+  account_id   = "sa-data-platform-${var.env}"
+  display_name = "Data Platform SA - ${var.env}"
+  project      = var.project_id
+}
+
+resource "google_project_iam_member" "bq_editor" {
+  project = var.project_id
+  role    = "roles/bigquery.dataEditor"
+  member  = "serviceAccount:${google_service_account.data_platform.email}"
+}
+
+resource "google_project_iam_member" "storage_admin" {
+  project = var.project_id
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.data_platform.email}"
+}
+
+resource "google_project_iam_member" "pubsub_publisher" {
+  project = var.project_id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${google_service_account.data_platform.email}"
+}
+
+resource "google_project_iam_member" "pubsub_subscriber" {
+  project = var.project_id
+  role    = "roles/pubsub.subscriber"
+  member  = "serviceAccount:${google_service_account.data_platform.email}"
+}
